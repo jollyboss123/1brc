6ee267f322072db064fca9684d69de88fd5503eb
jolly6 <98voonfusing@gmail.com>
2024-03-01 16:05:25 +0800

    first attempt

---
+++ prepare_first.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Uncomment below to use sdk
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 21.0.1-graal 1>&2
---
+++ src/main/java/org/jolly/onebrc/CalculateAverage_first.java
@@ -0,0 +1,58 @@
+package org.jolly.onebrc;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author jolly
+ */
+public class CalculateAverage_first {
+    private static final String MEASUREMENTS_FILE = "./measurements.txt";
+    record Result(double min, double max, double total, int count) {
+        private Result (double temp) {
+            this(temp, temp, temp, 1);
+        }
+
+        @Override
+        public String toString() {
+            double avg = Math.round((total * 100.0) / count) / 100.0;
+            return round(min) + "/" + round(avg) + "/" + round(max);
+        }
+
+        private double round(double val) {
+            return Math.round(val * 10.0) / 10.0;
+        }
+    }
+    record Measurement(String name, double temp) {
+        private Measurement(String[] arr) {
+            this(arr[0], Double.parseDouble(arr[1]));
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, Result> resultMap = new TreeMap<>();
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(MEASUREMENTS_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (var l : lines) {
+            final Measurement m = new Measurement(l.split(";", 2));
+            if (!resultMap.containsKey(m.name)) {
+                resultMap.put(m.name, new Result(m.temp));
+            } else {
+                Result r = resultMap.get(m.name);
+                Result u = new Result(Math.min(m.temp, r.min), Math.max(m.temp, r.max), r.total + m.temp, r.count + 1);
+                resultMap.replace(m.name, u);
+            }
+        }
+
+        System.out.println(resultMap);
+    }
+}
